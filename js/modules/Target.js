05ed5d95aeb70aa774b4c770cac91ae4f78addfd
nikolas <niiku_ch@hotmail.com>
2012-05-14 18:51:51 +0200

    Ziel.

--- js/modules/Target.js
+++ js/modules/Target.js
@@ -13,13 +13,13 @@ Crafty.c("Target",
 function getCastle(xa) {
     var hi = bs *5;
     var wi = bs * 5;
-    Crafty.e("2D, Canvas    , castle").attr({
+    Crafty.e("2D, Canvas, castle").attr({
         h:hi, 
         w:wi, 
         x:xa*(bs), 
         y:height-hi-(2*bs)
     });
-    Crafty.e("2D, Canvas, Color, Collision, Target").attr({
+    Crafty.e("2D, Canvas, Collision, Target").attr({
         h:2*bs, 
         w:bs, 
         x:2*bs+xa*(bs), 
