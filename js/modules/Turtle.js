8e6e734b048cfbeb0d93ad5ff620b153a01677e6
krenzaslv <krenzaslv@gmail.com>
2012-05-14 23:45:56 +0200

    *fixed turtle

--- js/modules/Turtle.js
+++ js/modules/Turtle.js
@@ -1,30 +1,32 @@
 Crafty.c("Turtle", {
-    transformed : false,
-    init : function() {
-        this.requires("ActionElement, iTurtle");
-        this.action = function(mario) {
-            if((mario._y + (mario._h - this._h) < this._y) || mario.isImmortal) {
-                mario._up
-                mario.score(10);
-                if(!this.transformed) {
-                    this.transformTurtle();
-                } else {
-                    this.destroy();
-                }
-            } else {
-                mario.die();
-            }
-        };
+	transformed : false,
+	init : function() {
+		this.requires("ActionElement, iTurtle");
+		this.action = function(mario) {
+			if((mario._y + (mario._h - this._h) < this._y) || mario.isImmortal) {
+				mario._up
+				mario.score(10);
+				if(!this.transformed) {
+					this.transformTurtle(mario);
+				} else {
+					this.destroy();
+				}
+			} else {
+				mario.die();
+			}
+		};
 
-    },
-    transformTurtle : function() {
-        this.toggleComponent("iTurtle", "iTurtleDown");
-        this.speed = 3;
-        this._gx = 50;
-        this.transformed = true;
-    }
+	},
+	transformTurtle : function(mario) {
+		mario._y -= 5;
+		mario._gy = 1;
+		this.toggleComponent("iTurtle", "iTurtleDown");
+		this.speed = 3;
+		this._gx = 50;
+		this.transformed = true;
+	}
 });
 function generateTurtle(x, y) {
-    var enemy = Crafty.e("Turtle");
-    enemy.position(x*bs, height-y*bs);
+	var enemy = Crafty.e("Turtle");
+	enemy.position(x * bs, height - y * bs);
 }
