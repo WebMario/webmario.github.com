94531f5d70bac599c8814df08b2f68fdc960e823
nikolas <niiku_ch@hotmail.com>
2012-05-14 22:31:39 +0200

    Cleaning

--- js/modules/FirTree.js
+++ js/modules/FirTree.js
@@ -1,13 +1,14 @@
 
 Crafty.c("FirTree", {
     init:function() {
+        this.requires("2D, Canvas, Solid, iFir")
     }
 });
 
 function generateSBFirTree(x, h) {
     for(var y = 0; y < h; y++) {
         for(var xa = x+y; xa < x+h; xa++) {
-            Crafty.e("2D, Canvas, Color, Collision, Solid, FirTree, iFir").attr({
+            Crafty.e("FirTree").attr({
                 h:bs, 
                 w:bs, 
                 x:xa*(bs), 
@@ -19,7 +20,7 @@ function generateSBFirTree(x, h) {
 function generateBSFirTree(x, h) {
     for(var y = h; y >= 0; y--) {
         for(var xa = x; xa < x+(h-y); xa++) {
-            Crafty.e("2D, Canvas, Color, Collision, Solid, FirTree, iFir").attr({
+            Crafty.e("FirTree").attr({
                 h:bs,
                 w:bs,
                 x:xa*(bs),
