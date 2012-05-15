07edb5a2100224bfbd1f4d0ae4363fc419f5f3db
janscheidegger <scheideggerj@gmail.com>
2012-05-15 14:39:05 +0300

    Comments

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+// Damit "." und ".." ausgeschlossen wird.
 function startsWith($haystack, $needle) {
 	$length = strlen($needle);
 	return (substr($haystack, 0, $length) === $needle);
 }
-
+// Fügt alle Dateien aus einem gewählten Ordner zwischen Script Tags
 function getJsFromDir($dir) {
      $handle=opendir ($dir);
         while ($datei = readdir ($handle)) {
