9608730dbe9fc5b1981df78b664239fefa48193d
janscheidegger <scheideggerj@gmail.com>
2012-05-15 14:13:58 +0300

    Gegner hinzugefügt, neue Elemente

--- js/modules/Map.js
+++ js/modules/Map.js
@@ -21,29 +21,38 @@ function generateMap() {
 	generatePipe(27, 2);
 	generatePipe(38, 3);
 	generatePipe(45, 4);
+	generateGnome(54,3);
 	generatePipe(55, 4);
         
-        generateCoin(62,8);
+    generateCoin(62,8);
 	generateActionBlock(62, 7);
 
 	generateFloor(68, 83);
 	generateAdobe(74, 6);
 	generateActionBlock(75, 6);
-        generateCoin(76,7);
+    generateCoin(76,7);
 	generateAdobe(76, 6);
 
 	generateFromTo(77, 10, 8, 1, generateAdobe);
+	generateFromTo(77, 11, 8, 1, generateCoin);
 
 	generateFloor(77, 83);
 
 	generateFloor(86, 148);
+	generatePipe(88,3);
+	generateGnome(94,3);
+	generateGnome(96,3);
+	generateGnome(98,3);
+	
+	generateAdobe(110,3);
+	generateTurtle(115,3);
 
 	generateFromTo(88, 10, 3, 1, generateAdobe);
 	generateActionBlock(91, 10);
-        generateCoin(91,7);
+    generateCoin(91,7);
 	generateActionBlock(91, 6);
         
-        generateCoin(76,7);
+    generateCoin(76,7);
 	generateAdobe(96, 6);
 
 	generateActionBlock(97, 6);
@@ -61,7 +70,7 @@ function generateMap() {
 	generateActionBlock(125, 10);
 	generateActionBlock(126, 10);
 	generateAdobe(127, 10);
-        generateCoin(125, 7);
+    generateCoin(125, 7);
 	generateAdobe(125, 6);
 	generateAdobe(126, 6);
 
@@ -81,10 +90,10 @@ function generateMap() {
 	generateAdobe(166, 6);
 	generateActionBlock(167, 6);
 	generateAdobe(168, 6);
-        generateCoin(176, 3);
-        generateCoin(176, 4);
-        generateCoin(176, 5);
-        generateCoin(176, 6);
+    generateCoin(176, 3);
+    generateCoin(176, 4);
+    generateCoin(176, 5);
+    generateCoin(176, 6);
 	generatePipe(176, 2);
 
 	generateSBFirTree(178, 9);
